a3a581f0e7fa688451a24fb58b554fe6da264e83
Hirofumi Kaneko <kaneko120@gmail.com>
2013-05-18 00:49:18 +0900

    Updated SublimeFeelingLucky

--- README.md
+++ README.md
@@ -1,6 +1,70 @@
 SublimeFeelingLucky
 ===================
 
-htmlファイルのidとclass名から関連してるcssファイル開いて良い感じにカーソルのフォーカス当ててくれるSublime Textのプラグイン
+SublimeFeelingLucky is Sublime Text Plugin which can open the related files of id or class of designation in html files.
 
-バグありありだけど
+![SublimeFeelingLucky](http://farm8.staticflickr.com/7293/8748074716_51763c0840_o.jpg)
+
+## Installation
+
+### Package Control
+
+using [Sublime Package Control](http://wbond.net/sublime_packages/package_control), you can easily install the SublimeFeelingLucky.
+
+````
+Sublime Text 2 -> Preferences -> Package Control: Install Package
+````  
+
+
+### GitHub
+Download the latest source from GitHub and copy the SublimeFeelingLucky folder to your Sublime Text `Packages` dir.
+
+### Git
+Clone the repository in your Sublime Text `Packages` dir
+
+````
+$ cd Packages
+$ git clone git@github.com:azzip/SublimeFeelingLucky.git
+````
+
+
+## config.feelinglucky
+Designation of `.css` and `.sass` target file.
+
+### Make
+1) Context Menu `Make config.feelinglucky`  
+2) Command short cut `super+ctrl+e`
+
+
+### Example
+
+````
+{
+    "css": [
+        "style.css"
+    ],
+    "sass": []
+}
+
+````
+
+## Usage
+Open Command Palette… `feeling lucky`
+
+````
+Tools -> Command Palette… -> feeling lucky
+````  
+
+
+## Default.sublime-keymap
+
+````
+[
+	{ "keys": ["ctrl+e"], "command": "feeling_lucky" },
+	{ "keys": ["super+ctrl+e"], "command": "make_config_dot_feeling_lucky" }
+]
+````
+
+## License
+
+MIT
--- SublimeFeelingLucky.py
+++ SublimeFeelingLucky.py
@@ -1,26 +1,30 @@
-import sublime, sublime_plugin, re, os, json
+#
+# SublimeFeelingLucky.py
+#
+# v 0.1
+#
+
+import sublime, sublime_plugin, re, os, json, time
 
 _word = ""
 _prefix = ""
 
-def _showAlert(message) :
-	sublime.error_message("SublimeFeelingLucky\n\n" + message)
-
-
-class FeelingLuckyCommand(sublime_plugin.TextCommand):
+class FeelingLucky(sublime_plugin.TextCommand):
 
 	def run(self, edit):
 		global _prefix
 		global _word
 
-		# load json
-		f = open(sublime.active_window().folders()[0] + "/config.feelinglucky")
-		data = json.load(f)
-		f.close()
-		print data
-		print data[0]['css'][0]
-		print data[0]['sass'][0]
+		projectPath = sublime.active_window().folders()[0]
 
+		if not _check(self, -4, "html") :
+			_showAlert("html file only")
+			return
+
+		# get Json data
+		data = self.loadJSON()
+		if not data :
+			return
 
 		for region in self.view.sel():
 			_word = self.view.substr(self.view.word(region))
@@ -32,39 +36,74 @@ class FeelingLuckyCommand(sublime_plugin.TextCommand):
 				return
 			else:
 				type = match.group(1)
-				if type == "id" 	 : _prefix = "#"
+				if   type == "id" 	 : _prefix = "#"
 				elif type == "class" : _prefix = "."
 
-			all = self.view.substr(sublime.Region(0, self.view.size()))
-			cssmatch = re.findall(r'href="(.*?.css)', all)
-			if cssmatch is None:
-				_showAlert("Not found css file")
-				return
-			else:
-				for file in cssmatch :
-					sublime.active_window().open_file(file)
+			count = 0
+
+			# check css
+			if "css" in data :
+				for css in data["css"] :
+					count += 1
+					if os.path.isfile(os.path.join(projectPath, css)) :
+
+						# TODO
+						# Already Open file move
+
+						self.view.window().run_command('expand_and_focus_right_panel', { "len": len(data["css"]), "count": count })
+						cssView = self.view.window().open_file(css)
+						cssView.window().set_view_index(cssView, count, 0)
+					else :
+						_printError("Not found " + css)
+
+			# check sass
+			if "sass" in data :
+				for sass in data["sass"] :
+					if os.path.isfile(os.path.join(projectPath, sass)) :
+						sublime.active_window().open_file(sass)
+					else :
+						_printError("Not found " + sass)
+
+
+	def loadJSON(self) :
+		try:
+			f = open(os.path.join(sublime.active_window().folders()[0], "config.feelinglucky"))
+			data = json.load(f)
+			f.close()
+			return data
+		except Exception, e:
+			if sublime.ok_cancel_dialog('Not found config.feelinglucky\nMake config.feelinglucky file ?') :
+				self.view.run_command("make_config_dot_feeling_lucky")
+			return False
 
 
 #
 # css file
 #
-class FeelingLuckyCssFileCommand(sublime_plugin.TextCommand):
+class FeelingLuckyCssFile(sublime_plugin.TextCommand):
 
 	def run(self, edit):
-		text = _prefix + _word
-		match = self.view.find(text, 0, sublime.LITERAL)
-		if not match:
+
+		if not _check(self, -3, "css") :
 			return
 
-		self.view.sel().clear()
-		self.view.sel().add(match)
-		self.view.show(match)
+		text = _prefix + _word
+		match = self.view.find(text, 0, sublime.LITERAL)
+		if match :
+			self.view.sel().clear()
+			self.view.sel().add(match)
+			self.view.show(match)
+
+		else :
+			# TODO
+			# not match file close
+			return
 
 
 #
 # event
 #
-class SublimeFeelingLuckyEventListener(sublime_plugin.EventListener):
+class FeelingLuckyEventListener(sublime_plugin.EventListener):
 
 	def on_load(self, view):
 		self.call(view)
@@ -73,14 +112,88 @@ class SublimeFeelingLuckyEventListener(sublime_plugin.EventListener):
 		self.call(view)
 
 	def call(self, view):
-		view.run_command('feeling_lucky_css_file')
+		view.run_command("feeling_lucky_css_file")
 
 
 #
 # config.feelinglucky
 #
-class MakeConfigDotFeelingLucky():
-	print 'Make config.feelingLucky'
+class MakeConfigDotFeelingLucky(sublime_plugin.TextCommand):
+
+	def run(self, edit):
+
+		if os.path.isfile(os.path.join(sublime.active_window().folders()[0], "config.feelinglucky")) :
+			_showAlert("Found config.feelingLucky")
+
+		else :
+			print 'Make config.feelingLucky'
+			config = { "sass":[], "css":["style.css"] }
+			file = open(os.path.join(sublime.active_window().folders()[0], "config.feelinglucky"), "w")
+			json.dump(config, file, indent=4)
+
+
+#
+# window controll
+#
+class ExpandAndFocusRightPanel(sublime_plugin.WindowCommand):
+
+	def run(self, len, count):
+		w = 0.6;
+
+		if len == 1:
+			p = {
+					"cols": [0.0, w, 1.0],
+					"rows": [0.0, 1.0],
+					"cells":
+					[
+						[0, 0, 1, 1], [1, 0, 2, 1]
+					]
+				}
+		elif len == 2 :
+			p = {
+					"cols": [0.0, w, 1.0],
+					"rows": [0.0, 0.5, 1.0],
+					"cells":
+					[
+					    [0, 0, 1, 2], [1, 0, 2, 1],
+					                  [1, 1, 2, 2]
+					]
+				}
+		elif len >= 3 :
+			p = {
+					"cols": [0.0, w, 1.0],
+					"rows": [0.0, 0.33, 0.66, 1.0],
+					"cells":
+					[
+					    [0, 0, 1, 3], [1, 0, 2, 1],
+					                  [1, 1, 2, 2],
+					                  [1, 2, 2, 3]
+					]
+				}
+
+		self.window.run_command("set_layout", p)
+		self.window.run_command("focus_group", {"group": count})
+
+
+#
+# utils
+#
+
+def _showAlert(message) :
+	sublime.error_message("SublimeFeelingLucky : \n" + message)
+
+def _printError(message) :
+	print "[FeelingLucky Error] : " + message
+
+# file type check
+def _check(self, range, type) :
+	fileName = self.view.file_name()
+	if fileName[range:] == type :
+		return True
+	else :
+		return False
+
+
 
 
 
